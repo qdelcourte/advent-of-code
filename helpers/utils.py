9b5fd6893e9aa49700391ec2fc567d37835659fe
Quentin Delcourte <delcourte.quentin@hotmail.com>
2025-10-01 13:38:56 +0200

    add text colors helpers

--- helpers/utils.py
+++ helpers/utils.py
@@ -15,3 +15,13 @@ def pprint_matrix(m):
 
 def str_to_matrix(data: str):
     return [list(map(int, line)) for line in data]
+
+# Colors
+red = lambda s: "\033[91m {}\033[00m".format(s)
+green = lambda s: "\033[92m {}\033[00m".format(s)
+yellow = lambda s: "\033[93m {}\033[00m".format(s)
+light_purple = lambda s: "\033[94m {}\033[00m".format(s)
+purple = lambda s: "\033[95m {}\033[00m".format(s)
+cyan = lambda s: "\033[96m {}\033[00m".format(s)
+light_gray = lambda s: "\033[97m {}\033[00m".format(s)
+black = lambda s: "\033[90m {}\033[00m".format(s)  # Corrected from 98 to 90 (standard ANSI)
