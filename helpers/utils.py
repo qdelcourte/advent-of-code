2f192ba858a8a6d77f3fda1f8614d08e2f46afd0
Quentin Delcourte <delcourte.quentin@hotmail.com>
2025-10-18 15:02:14 +0200

    Improve Colors utils - random, enumerate

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def read_input_from_main(execution_file: str):
     return open(os.path.join(os.path.dirname(execution_file), "input.txt"), 'r').read().splitlines()
@@ -20,11 +21,64 @@ def flatten(list_of_lists):
     return [x for xs in list_of_lists for x in xs]
 
 class Colors:
-    red = lambda s: "\033[91m {}\033[00m".format(s)
-    green = lambda s: "\033[92m {}\033[00m".format(s)
-    yellow = lambda s: "\033[93m {}\033[00m".format(s)
-    light_purple = lambda s: "\033[94m {}\033[00m".format(s)
-    purple = lambda s: "\033[95m {}\033[00m".format(s)
-    cyan = lambda s: "\033[96m {}\033[00m".format(s)
-    light_gray = lambda s: "\033[97m {}\033[00m".format(s)
-    black = lambda s: "\033[90m {}\033[00m".format(s)  # Corrected from 98 to 90 (standard ANSI)
+    red = lambda s: "\033[91m{}\033[00m".format(s)
+    green = lambda s: "\033[92m{}\033[00m".format(s)
+    yellow = lambda s: "\033[93m{}\033[00m".format(s)
+    light_purple = lambda s: "\033[94m{}\033[00m".format(s)
+    purple = lambda s: "\033[95m{}\033[00m".format(s)
+    cyan = lambda s: "\033[96m{}\033[00m".format(s)
+    light_gray = lambda s: "\033[97m{}\033[00m".format(s)
+    black = lambda s: "\033[90m{}\033[00m".format(s)  # Corrected from 98 to 90 (standard ANSI)
+
+    @classmethod
+    def list(cls):
+        return [
+            attr for attr_name, attr in cls.__dict__.items()
+            if callable(attr) and not attr_name.startswith("__")
+        ]
+
+    @classmethod
+    def random(cls, text):
+        """
+        Applies a randomly selected color function to the given text.
+
+        Parameters
+        ----------
+        text : str
+            The text to which the randomly chosen color function will be applied.
+
+        Returns
+        -------
+        str
+            The input text formatted with a randomly selected color function
+
+        Example
+        -------
+        >>> Colors.random("Hello")
+        '\033[94mHello\033[00m'   # text displayed in a random color
+        """
+        return random.choice(cls.list())(text)
+
+    @classmethod
+    def enumerate(cls, iterable, start=0):
+        """
+        Enumerates over `iterable`, pairing each element with a color function from
+        the class's color list, cycling through the colors.
+
+        Yields
+        ------
+        tuple
+            (color_function, index, item)
+
+        Example
+        -------
+        >>> for color_func, idx, val in Colors.enumerate(["a", "b", "c"]):
+        ...     print(color_func(val))
+        [RED]a[/RED]
+        [GREEN]b[/GREEN]
+        [RED]c[/RED]
+        """
+        colors = cls.list()
+        n = len(colors)
+        for i, item in enumerate(iterable, start):
+            yield colors[i % n], i, item
